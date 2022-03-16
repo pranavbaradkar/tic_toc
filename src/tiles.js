612617a3bcd40700e7a7afebf6ae90a08169bb65
pranavbaradkar <pyb11@iitbbs.ac.in>
2022-03-16 19:25:42 +0530

    init

--- src/tiles.js
+++ src/tiles.js
@@ -4,7 +4,7 @@ import tile from './tiles.module.css'
 
 const Tiles = function () {
       const[count,setcount] = useState(0) ; 
-      const [blocks,setblocks] = useState([
+      const [blocks,setblocks,getblock] = useState([
           {
                id:1, 
                row: 0,
@@ -72,19 +72,36 @@ const Tiles = function () {
 
      ]);
      const [original,setoriginal] = useState(true) ; 
+     const[index,setIndex] = useState(-1) ; 
+     const [toggle,settoggle] = useState(true) ; 
+     const [win,setWin] = useState(false) ; 
+     const[over,setover] = useState(false) ; 
 
      useEffect(()=>{
         setoriginal(blocks) ; 
-     },[count]) ; 
+        setWin(false) ; 
+        settoggle(true) ; 
+        setover(false); 
+     },[count]) ;
+
+     useEffect(()=> {
+          if (index !== -1) {
+              settoggle(prevToggle => !prevToggle) ; 
+              let value = (toggle) ? "O" : "X" ; 
+              let chck = check(blocks,blocks[index].row,blocks[index].column,value) ; 
+              if (chck) {
+                  setWin(true) ;  
+              }
+              console.log(blocks);
+          }
+     },[blocks]); 
 
 
      // const origine = [...blocks] ; 
      // console.log(blocks) ; 
-     const [toggle,settoggle] = useState(true) ; 
-     const [win,setWin] = useState(false) ; 
 
      const check = function(array,row,column,value) {
-          let r = 0,c = 0,d1 = 0,d2 = 0 ; 
+          let r = 0,c = 0,d1 = 0,d2 = 0,total = 0 ; 
           for (let box of array) {
                if (box.row === row && box.value === value) {
                     r++; 
@@ -98,37 +115,36 @@ const Tiles = function () {
                if ((box.row === (3 - box.column - 1)) && box.value === value) {
                     d2++; 
                }
+               if (box.value !== "") {
+                    total++ ; 
+               }
           }
           console.log(r,c,d1,d2)
-          if (r === 2 || c === 2 || d1 === 2 || d2 === 2) {
+          if (r === 3 || c === 3 || d1 === 3 || d2 === 3) {
                return true; 
           }
+          console.log("totgal->>>",total) ; 
+          if (total === 9) {
+              setover(true) ; 
+          }
+
           return false ; 
           
      }
 
 
      const changevalue = function(index) {
+          setIndex(index) ; 
           if (blocks[index].clicked) {
                return ; 
           }
           let value = (toggle) ? "O" : "X" ; 
-          setblocks(blocks.map(item => item.id === index+1 ? {...item,value : value,clicked:true} : item)) ; 
-          // console.log(blocks) ; 
-          let chck = check(blocks,blocks[index].row,blocks[index].column,value) ; 
-          // console.log("Win ---- ",win) ; 
-          // console.log("chck", chck) ; 
-          if (chck) {
-              setWin(true) ;  
-          }
-          settoggle(prevToggle => !prevToggle) ; 
+          setblocks(prevBlocks => prevBlocks.map(item => item.id === index+1 ? {...item,value : value,clicked:true} : item)) ; 
      }
 
      const reset = function() {
-          setWin(false) ; 
-          setblocks(original) ; 
           setcount(count => count + 1) ; 
-          settoggle(prevToggle => !prevToggle)
+          setblocks(original) ; 
      }
 
       return (
@@ -144,9 +160,13 @@ const Tiles = function () {
            })}
            </div>
            {win && <div className={tile.popUp}>
-                <h1>{!toggle ? "1st player win" : "2nd player win" }</h1>
+                <h1>{!toggle ? "1st player win" : "2nd player win"}</h1>
                 <button onClick={() => reset()}>Start New Game</button>
                 </div>}
+          {over && <div className={tile.popUp}>
+                <h1>Game Over</h1>
+                <button onClick={() => reset()}>Start New Game</button>
+          </div>}
 
       </div>
     );
